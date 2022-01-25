b8c23931ab3c310484f5b1cd7f9cd814bcac5dd3
Md Rakib Sarker <87212579+rakibsarker24@users.noreply.github.com>
2022-01-25 15:53:46 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+
+$(function () {
+
+
+  $('.slider-main').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    dots: true,
+    arrows: false,
+  });
+      
+
+
+
+
+
+
+});
+
